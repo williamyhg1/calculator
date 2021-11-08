6024d878b79549ab91785077a14ceaaad11dc1a6
williamyhg1 <90809764+williamyhg1@users.noreply.github.com>
2021-11-08 16:04:25 +1100

    revise html

--- script.js
+++ script.js
@@ -1,89 +1,24 @@
-const display = document.getElementById('display');
-const btnClear = document.getElementById('clear');
-btnClear.addEventListener('click', ()=>window.location.reload());
+const previousOperand = document.querySelector('[data-previous-operand]');
+const currentOperand = document.querySelector('[data-current-operand]')
+const btnAllClear = document.getElementById('allclear');
+btnAllClear.addEventListener('click', ()=>{
+    currentOperand.innerText = '';
+    previousOperand.innerText ='';
+});
+
+
+const numberBtns = document.querySelectorAll('[data-number]')
+numberBtns.forEach(btn=>btn.addEventListener('click', () => {
+    if(btn.innertext === '.' && currentOperand.innerText.includes('.')){return}else{currentOperand.innerText += btn.textContent}
+}));
+
 const btnDelete = document.getElementById('delete');
-btnDelete.addEventListener('click', ()=>display.removeChild(display.lastChild));
+btnDelete.addEventListener('click', () => {
+    currentOperand.innerText = currentOperand.innerText.slice(0,-1)
+});
 
-function operation(){
-const numbers = document.querySelectorAll()
-
-}
-// const btnDelete 
-// display.textContent = 'Test'
-// display.addEventListener('')
-// const btnOne = document.getElementById('one') 
-// const btnTwo = document.getElementById('two') 
-// const btnThree = document.getElementById('three') 
-// const btnFour = document.getElementById('four') 
-// const btnFive = document.getElementById('five') 
-// const btnSix = document.getElementById('six') 
-// const btnSeven = document.getElementById('seven') 
-// const btnEight = document.getElementById('eight') 
-// const btnNine = document.getElementById('nine') 
-// const btnZero = document.getElementById('zero') 
-// const btnDivide = document.getElementById('divide')
-// const btnMultiply = document.getElementById('multiply') 
-// const btnMinus = document.getElementById('minus') 
-// const btnPlus = document.getElementById('plus') 
-// const btnDecimal = document.getElementById('decimal') 
-
-
-
-const btns = document.querySelectorAll('.btn')
-btns.forEach(btn=>btn.addEventListener('click', toDisplay))
-
-function toDisplay(e){
-    const newText = document.createElement('div');
-    switch (e.target.id){
-        case 'one':
-            newText.textContent = 1;
-            break;
-        case 'two':
-            newText.textContent = 2;
-            break;
-        case 'three':
-            newText.textContent = 3;
-            break;
-        case 'four':
-            newText.textContent = 4;
-            break;
-        case 'five':
-            newText.textContent = 5;
-            break;
-        case 'six':
-            newText.textContent = 6;
-            break;
-        case 'seven':
-            newText.textContent = 7;
-            break;
-        case 'eight':
-            newText.textContent = 8;
-            break;
-        case 'nine':
-            newText.textContent = 9;
-            break;
-        case 'zero':
-            newText.textContent = 0;
-            break;
-        case 'plus':
-            newText.textContent = '+';
-            break;
-        case 'minus':
-            newText.textContent = '-';
-            break;
-        case 'multiply':
-            newText.textContent = '*';
-            break;
-        case 'divide':
-            newText.textContent = '/';
-            break;
-        case 'decimal':
-            newText.textContent = '.';
-            break;
-        case 'equal':
-            newText.textContent = '=';
-            break;
-   }
-   display.append(newText)
-
-}
+const operationBtns = document.querySelectorAll('[data-operator]')
+operationBtns.forEach(btn=>btn.addEventListener('click',()=>{
+    previousOperand.innerText = currentOperand.innerText;
+    currentOperand.innerText =''
+}))
